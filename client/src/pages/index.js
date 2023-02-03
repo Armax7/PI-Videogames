7a0249f2ed10d23073c1950900c33fd13892e2ae
Armando Isaac Garcia <armando7jx@gmail.com>
2023-02-03 17:18:16 -0600

    Pages first commit

---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export {default as Landing} from './Landing/Landing';
+export {default as Home} from './Home/Home';
+export {default as Form} from './Form/Form';
+export {default as Detail} from './Detail/Detail';
