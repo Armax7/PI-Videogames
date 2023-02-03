d7d8d65023d62ba34193c8cf1c429b44e44df73c
Armando Isaac Garcia <armando7jx@gmail.com>
2023-02-03 17:15:26 -0600

    db.js 2nd commit, DB relations

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: "../.env"});
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
@@ -6,7 +6,6 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_PORT,DB_NAME,
 } = process.env;
 
-console.log(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`)
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
