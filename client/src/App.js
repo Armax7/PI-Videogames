4d1ed1cc65e4ae8ec17e4f68ab6f3eb6938772c0
Armando Isaac Garcia <armando7jx@gmail.com>
2023-02-03 17:19:14 -0600

    App first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import * as pages from './pages/index.js'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+    
+    <BrowserRouter>
+      <div className="App">
+          <Switch>
+            <Route exact path="/" component={pages.Landing} />
+            <Route exact path={'/home'} component={pages.Home} />
+            <Route exact path={'/detail'} component={pages.Detail} />
+            <Route exact path={'/form'} component={pages.Form} />
+          </Switch>
+      </div>
+    </BrowserRouter>
+      
   );
 }
 
