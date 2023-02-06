c40ac23526ddd0ac7a8cf8edde7f56ec79c74dc4
Armando Isaac Garcia <armando7jx@gmail.com>
2023-02-06 12:22:15 -0600

    Home 2nd commit

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { Link } from 'react-router-dom';
+import * as actions from '../../redux/actions';
 
 function Home() {
+  const dispatch = useDispatch();
+  const allVideogames = useSelector(state => state.videogame_list);
+
+  useEffect(()=>{
+    dispatch(actions.getVideogames());
+  },[]);
+
   return (
-    <div>Home</div>
+    <div>
+      <Link to={'/videogames'} >Add videogame</Link>
+      <button>Reload</button>
+    </div>
   )
 }
 
