8701198e3f4c7be06fd3ff6070a7c57753e6599b
Armando Isaac Garcia <armando7jx@gmail.com>
2023-02-13 15:04:36 -0600

    Redux 2nd commit

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,10 +1,58 @@
 import axios from 'axios';
 
 export const GET_VIDEOGAMES = 'GET_VIDEOGAMES';
+export const GET_GENRES = 'GET_GENRES';
+export const FILTER_BY_GENRE = 'FILTER_BY_GENRE';
+export const FILTER_BY_ORIGIN = 'FILTER_BY_ORIGIN';
+export const FILTER_ALL_GAMES = 'FILTER_ALL_GAMES';
+export const SORT_BY_NAME = 'FILTER_BY_NAME';
+export const SORT_BY_RATING = 'SORT_BY_RATING';
 
 export function getVideogames(){
     return async function (dispatch){
-        const videogames = await axios.get('http://localhost:3001/videogames');
-        return dispatch({type: GET_VIDEOGAMES, payload: videogames.data});
+        const response = await axios.get('http://localhost:3001/videogames');
+        return dispatch({type: GET_VIDEOGAMES, payload: response.data});
+    }
+}
+
+export function getGenres(){
+    return async function (dispatch) {
+        const response = await axios.get('http://localhost:3001/genres');
+        return dispatch({type: GET_GENRES, payload: response.data})
+    }
+}
+
+export function filterGameByGenre (payload) {
+    return {
+        type: FILTER_BY_GENRE,
+        payload,
+    }
+}
+
+export function filterGameByOrigin(payload){
+    return {
+        type: FILTER_BY_ORIGIN,
+        payload,
+    }
+}
+
+export function filterAllGames () {
+    return {
+        type: FILTER_ALL_GAMES,
+        payload: FILTER_ALL_GAMES,
+    }
+}
+
+export function sortByName(payload){
+    return {
+        type: SORT_BY_NAME,
+        payload,
+    }
+}
+
+export function sortByRating(payload){
+    return {
+        type: SORT_BY_RATING,
+        payload,
     }
 }
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,16 +1,118 @@
-import * as actions from './actions'
+import * as Actions from './actions';
+import * as Constant from '../constants';
+import * as Utils from '../utils';
+// import _ from 'lodash';
 
 const initialState = {
     videogame_list: [],
+    videogamesByGenre: [],
+    videogamesByOrigin: [],
+    allVideogames: [],
     videogame: {},
+    genre_list:[],
 }
+
 
 export default function rootReducer (state = initialState, action) {
     switch(action.type) {
-        case actions.GET_VIDEOGAMES:
+        case Actions.GET_VIDEOGAMES:
+            const initialRes = action.payload;
             return {
                 ...state,
-                videogame_list: action.payload,
+                videogame_list: initialRes.sort((a,b)=>(Utils.mySortAscendingComparingFunc(a.name,b.name))),
+                videogamesByGenre: initialRes.sort((a,b)=>(Utils.mySortAscendingComparingFunc(a.name,b.name))),
+                videogamesByOrigin: initialRes.sort((a,b)=>(Utils.mySortAscendingComparingFunc(a.name,b.name))),
+                allVideogames: initialRes.sort((a,b)=>(Utils.mySortAscendingComparingFunc(a.name,b.name))),
             }
+
+        case Actions.GET_GENRES:
+            return {
+                ...state,
+                genre_list: action.payload,
+            }
+
+        case Actions.FILTER_BY_GENRE:
+            let filteredByGenre = state.videogamesByOrigin;
+            let allByGenre =  state.allVideogames;
+            if(action.payload !== Constant.ALL){
+                allByGenre = allByGenre.filter(elem => elem.genres.some(el => el.name === action.payload));
+                filteredByGenre = filteredByGenre.filter(elem => elem.genres.some(el => el.name === action.payload));
+            }
+            return {
+                ...state,
+                videogame_list: filteredByGenre,
+                videogamesByGenre: allByGenre,
+            }
+
+        case Actions.FILTER_BY_ORIGIN:
+            let allByOrigin = state.allVideogames;
+            let filteredByOrigin = state.videogamesByGenre;
+            if(action.payload === Constant.EXISTING_GAME) {
+                allByOrigin = allByOrigin.filter(elem => !elem.created);
+                filteredByOrigin = filteredByOrigin.filter(elem => !elem.created)
+            }
+            else if (action.payload === Constant.CREATED_GAME) {
+                allByOrigin = allByOrigin.filter(elem => elem.created);
+                filteredByOrigin = filteredByOrigin.filter(elem => elem.created)
+            }
+            return {
+                ...state,
+                videogame_list: filteredByOrigin,
+                videogamesByOrigin: allByOrigin,
+            }
+        
+        case Actions.FILTER_ALL_GAMES:
+            return {
+                ...state,
+                videogame_list: state.allVideogames.sort((a,b)=>(Utils.mySortAscendingComparingFunc(a.name,b.name))),
+                videogamesByGenre: state.allVideogames.sort((a,b)=>(Utils.mySortAscendingComparingFunc(a.name,b.name))),
+                videogamesByOrigin: state.allVideogames.sort((a,b)=>(Utils.mySortAscendingComparingFunc(a.name,b.name))),
+            }
+
+        case Actions.SORT_BY_NAME:
+            let sortedByName = state.videogame_list;
+            let sortedGenreByName  = state.videogamesByGenre;
+            let sortedOriginByName = state.videogamesByOrigin;
+            if(action.payload === Constant.ASCENDING){
+                sortedByName.sort((a,b)=>Utils.mySortAscendingComparingFunc(a.name,b.name));
+                sortedGenreByName.sort((a,b)=>Utils.mySortAscendingComparingFunc(a.name,b.name));
+                sortedOriginByName.sort((a,b)=>Utils.mySortAscendingComparingFunc(a.name,b.name));
+            }
+            else if(action.payload === Constant.DESCENDING){
+                sortedByName.sort((a,b)=>Utils.mySortDescendingComparingFunc(a.name,b.name));
+                sortedGenreByName.sort((a,b)=>Utils.mySortDescendingComparingFunc(a.name,b.name));
+                sortedOriginByName.sort((a,b)=>Utils.mySortDescendingComparingFunc(a.name,b.name));
+            }
+            // console.log(state.allVideogames)
+            return {
+                ...state,
+                videogame_list: sortedByName,
+                videogamesByGenre:  sortedGenreByName,
+                videogamesByOrigin: sortedOriginByName,
+            }
+
+        case Actions.SORT_BY_RATING:
+            let sortedByRating = state.videogame_list;
+            let sortedGenreByRating  = state.videogamesByGenre;
+            let sortedOriginByRating = state.videogamesByOrigin;
+            if(action.payload === Constant.ASCENDING){
+                sortedByRating.sort((a,b)=>Utils.mySortAscendingComparingFunc(a.rating, b.rating));
+                sortedGenreByRating.sort((a,b)=>Utils.mySortAscendingComparingFunc(a.rating, b.rating));
+                sortedOriginByRating.sort((a,b)=>Utils.mySortAscendingComparingFunc(a.rating, b.rating));
+            }
+            else if(action.payload === Constant.DESCENDING){
+                sortedByRating.sort((a,b)=>Utils.mySortDescendingComparingFunc(a.rating, b.rating));
+                sortedGenreByRating.sort((a,b)=>Utils.mySortDescendingComparingFunc(a.rating, b.rating));
+                sortedOriginByRating.sort((a,b)=>Utils.mySortDescendingComparingFunc(a.rating, b.rating));
+            }
+            // console.log(state.allVideogames)
+            return {
+                ...state,
+                videogame_list: sortedByRating,
+                videogamesByGenre:  sortedGenreByRating,
+                videogamesByOrigin: sortedOriginByRating,
+            }
+
+        default: return {...state};
     }
 }
