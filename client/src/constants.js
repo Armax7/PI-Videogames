d3f1a53fb42bf94d1858a611c3955bae6027ef3b
Armando Isaac Garcia <armando7jx@gmail.com>
2023-02-13 15:13:27 -0600

    Constants 1st commit

---
+++ client/src/constants.js
@@ -0,0 +1,9 @@
+
+export const ALL = 'All';
+export const NONE = 'None';
+export const EXISTING_GAME = 'Existing Game'; 
+export const CREATED_GAME = 'Created Game';
+export const ALPHABETICAL = 'Alphabetical';
+export const RATING = 'Rating';
+export const DESCENDING = 'Descending';
+export const ASCENDING = 'Ascending';
